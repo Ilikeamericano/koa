4d71dcb27e66b21a513fa18e9260fb7eaf077b7d
leesungtaek <sungtaek13@naver.com>
2022-10-14 08:50:45 +0900

    second commit

--- page/intro.php
+++ page/intro.php
@@ -7,6 +7,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 	.page-content p { margin:0 0 15px; padding:0; }
 	.page-content .slogan { font-size:25px; letter-spacing:-1px; margin-bottom:15px; line-height:34px; }
 	.page-content .slogan i { font-size:17px; vertical-align:top; margin-top:6px; }
+	
 </style>
 
 <div class="page-content">
